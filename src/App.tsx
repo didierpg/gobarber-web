3022b9bd9f20a65e600c119df1de2cb9674ccf87
Didier <didierpg@gmail.com>
2020-06-08 20:48:37 -0300

    Iniciando front-end web ▸ Estrutura das páginas  ▸ Página de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
+import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
