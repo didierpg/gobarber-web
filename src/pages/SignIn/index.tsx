3a5e4f8122d17c75872fb944f5155b5093da3d21
Didier <didierpg@gmail.com>
2020-06-17 23:17:00 -0300

    Iniciando front-end web ▸ Autenticação  ▸ Logout da aplicação

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -11,7 +11,7 @@ import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 interface SignInFormData {
