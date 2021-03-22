23f86046b6abda62add4ed47f57ce4d2aa8a72c5
Didier <didierpg@gmail.com>
2021-03-21 23:09:22 -0300

    Finalizando front-end web do app ▸ Perfil do usuário ▸ Página de perfil

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,11 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-
-import Dashboard from '../pages/Dashboard';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -18,6 +19,7 @@ const Routes: React.FC = () => (
     <Route path="/reset-password" component={ResetPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
+    <Route path="/profile" component={Profile} isPrivate />
   </Switch>
 );
 
